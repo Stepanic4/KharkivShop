3d268aec66563049a5a4be3104621063014faaac
Ivan <Zolotukhin.ivan.1@gmail.com>
2017-09-20 13:35:09 +0300

    Working out
    strat

---
+++ js/custom-scripts.js
@@ -0,0 +1,21 @@
+
+/////////////////////////////////////////////////////////// Плавный скрол по сайту /////////////////////////////////////
+
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+
+
+///////////////////////////////////////////////////   BURGER  ///////////////////////////////////////////////////////////////////
+    
+
+	$('.burger').click(function() {
+		$('.navbar-collapse').toggleClass('active');
+	});
+
+	
